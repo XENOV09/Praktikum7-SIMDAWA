ca8b420ed29f30885d0097044d27bbc6288757d1
XENOV09 <novriyan09@gmail.com>
2025-01-13 21:45:01 +0800

    Pertemuan 4 Membuat Halaman Pendaftaran, Login, dan Pembatasan Akses

--- application/views/prodi/prodi_print.php
+++ application/views/prodi/prodi_print.php
@@ -15,16 +15,10 @@ $pdf->SetFont("Arial", "B", 11);
 $pdf->SetFillColor(0, 255, 0);
 $pdf->Cell(3, 1, "No", 1, 0, "C", true);
 $pdf->Cell(15, 1, "Nama Program Studi", 1, 1, "C", true);
-//$pdf->Cell(12, 1, "Keterangan", 1, 1, "C", true);
-// $pdf->Cell(4, 1, "Tanggal Selesai", 1, 0, "C", true);
-// $pdf->Cell(4, 1, "Program Studi", 1, 1, "C", true);
 $pdf->SetFont("Arial", "", 11);
 $no = 1;
 foreach ($prodi as $a) {
     $pdf->Cell(3, 1, $no++, 1, 0, "C");
     $pdf->Cell(15, 1, $a->nama_prodi, 1, 1, "C");
-    // $pdf->Cell(12, 1, $a->keterangan, 1, 1, "C");
-    // $pdf->Cell(4, 1, $a->nama_jenis, 1, 0, "C");
-    // $pdf->Cell(4, 1, $a->nama_jenis, 1, 1, "C");
 }
 $pdf->Output("I", "Laporan Data Program Studi.pdf");
